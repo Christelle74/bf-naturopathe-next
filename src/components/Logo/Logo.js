061e76f5aa84169ff020f8d70853535b38d33d53
ChristellePhilippe <c.philippe04@hotmail.com>
2023-02-15 17:20:50 +0100

    modif eslintrc

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import Image from "next/legacy/image"
 import logo from '../../../public/logo.png'
+import Link from "next/link"
 import styles from './Logo.module.css'
 
 
 export default function Logo() {
     return (
         <div className={styles.logo}>
-            <a href='/'><Image layout="intrinsic" src={logo} width={678} height={678} alt="Beatrice Fabry naturopathe"/></a>
+            <Link href='/'><Image layout="intrinsic" src={logo} width={678} height={678} alt="Beatrice Fabry naturopathe"/></Link>
         </div>
     )
 }
